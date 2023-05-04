403f7bf4ca02d36b9b828bf4808e93f1fd1a06ba
mr-duk25 <samuelrobles@Samuels-MacBook-Pro.local>
2023-05-03 21:44:35 -0700

    + CSS link

--- controllers/paths.js
+++ controllers/paths.js
@@ -22,11 +22,5 @@ function index(req, res) {
     });
   }
 
-  function getOne (id) {
-    //url params are strings - convert to numbers
-    id = parseInt(id);
-    // The Array.prototype.find iterator method is
-    // ideal for finding objects within an array
-    return paths.find(path => path.id === id);
-  }
+ 
   
